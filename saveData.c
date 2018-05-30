d2c25dd74c2498736b3751bb53dfb8d1e6d2c402
Augusto Maza <debianmaza@gmail.com>
2018-05-29 23:03:58 -0300

    funcion guardar funciona

--- saveData.c
+++ saveData.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "funciones.h"
 
 #define TAM 2
 
-void saveData (EMovie[], int cant)
+void saveData (EMovie movie[], int cant)
 {
 
     FILE *save;
 
-
-
     save = fopen("listaPeliculas.txt","w");
-    fwrite(&movie,sizeof(EMovie),cant,*save);
+    fwrite(&movie,sizeof(EMovie),cant,save);
     fclose(save);
 
 
